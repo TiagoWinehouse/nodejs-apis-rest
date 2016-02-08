fd242cb6bbdd597c9dae77259f4b54ec89005b9b
Tiago Nunes <tiagowinehouse@gmail.com>
2016-02-08 11:40:46 -0300

    :sunny: Corrigindo o erro no Test token.js :sweat_smile:

--- nTask-api/routes/token.js
+++ nTask-api/routes/token.js
@@ -9,22 +9,21 @@ module.exports = app => {
       const email = req.body.email;
       const password = req.body.password;
       Users.findOne({
-          where: {
-            email: email
-          }
-        }).then(user => {
-          if (Users.isPassword(user.password, password)) {
-            const payload = {
-              id: user.id
-            };
-            res.json({
-              token: jwt.encode(payload, cfg.jwtSecret)
-            });
-          } else {
-            res.sendStatus(401);
-          }
-        })
-        .catch(error => res.sendStatus(401));
+        Where: {
+          email: email
+        }
+      }).then(user => {
+        if (Users.isPassword(user.password, password)) {
+          const payload = {
+            id: user.id
+          };
+          res.json({
+            token: jwt.encode(payload, cfg.jwtSecret)
+          });
+        } else {
+          res.sendStatus(401);
+        }
+      }).catch(error => res.sendStatus(401));
     } else {
       res.sendStatus(401);
     }
--- nTask-api/test/routes/token.js
+++ nTask-api/test/routes/token.js
@@ -14,54 +14,55 @@ describe("Routes: Token", () => {
         }))
         .then(done());
     });
+  });
 
-    describe("status 200", () => {
-      it("returns authenticated user token", done => {
-        request.post("/token")
-          .send({
-            email: "john@mail.net",
-            password: "12345"
-          })
-          .expect(200)
-          .end((err, res) => {
-            expect(res.body).to.include.keys("token");
-            done(err);
-          });
-      });
+  describe("Teste Status 200", () => {
+    it("returns authenticated user token", done => {
+      request.post("/token")
+        .send({
+          email: "john@mail.net",
+          password: "12345"
+        })
+        .expect(200)
+        .end((err, res) => {
+          // console.log(res);
+          expect(res.body).to.include.keys("token");
+          done(err);
+        });
+    });
+  });
+
+  describe("status 401", () => {
+    it("throws error when password is incorrect", done => {
+      request.post("/token")
+        .send({
+          email: "john@mail.net",
+          password: "SENHA_ERRADA"
+        })
+        .expect(401)
+        .end((err, res) => {
+          done(err);
+        });
     });
 
-    describe("status 401", () => {
-      it("throws error when password is incorrect", done => {
-        request.post("/token")
-          .send({
-            email: "john@mail.net",
-            password: "SENHA_ERRADA"
-          })
-          .expect(401)
-          .end((err, res) => {
-            done(err);
-          });
-      });
+    it("throws error when email not exist", done => {
+      request.post("/token")
+        .send({
+          email: "EMAIL_ERRADO",
+          password: "SENHA_ERRADA"
+        })
+        .expect(401)
+        .end((err, res) => {
+          done(err);
+        });
+    });
 
-      it("throws error when email not exist", done => {
-        request.post("/token")
-          .send({
-            email: "EMAIL_ERRADO",
-            password: "SENHA_ERRADA"
-          })
-          .expect(401)
-          .end((err, res) => {
-            done(err);
-          });
-      });
-
-      it("throws error when email and password are blank", done => {
-        request.post("/token")
-          .expect(401)
-          .end((err, res) => {
-            done(err);
-          });
-      });
+    it("throws error when email and password are blank", done => {
+      request.post("/token")
+        .expect(401)
+        .end((err, res) => {
+          done(err);
+        });
     });
   });
 });
